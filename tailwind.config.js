56bab640f6bf9a7cfb6488c93b77c6074fbdbeec
Amar-Omerika <72925790+Amar-Omerika@users.noreply.github.com>
2023-09-18 14:00:58 +0200

    Added tailwind configuration to project

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                green: '#438096',
+                red: '#CE5C60',
+                lightBlue: '#23A2CE',
+                orange: '#E3A73C',
+                blue: '#3762A9',
+                purple: '#624590',
+                lightGrey: '#E6E8E9',
+                grey: '#707071',
+                pink: '#B85797'
+            }
+        }
+    },
+    plugins: []
+}
