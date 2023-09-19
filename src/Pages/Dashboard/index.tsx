7656a32e51f07d2be451e0c829d04f9c23cca6ed
Amar-Omerika <72925790+Amar-Omerika@users.noreply.github.com>
2023-09-19 16:38:16 +0200

    Finished area chart, optimized and refactored code

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,13 +1,124 @@
 import React from "react";
 import { AreaChart } from "../../components";
+import { faker } from "@faker-js/faker";
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+const OPTIONS_PLACEHOLDER = [
+  {
+    data: {
+      labels,
+      datasets: [
+        {
+          display: false,
+          fill: true,
+          label: "Orders",
+          data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+          backgroundColor: "rgba(206, 92, 96, 1)",
+        },
+      ],
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    height: "100%",
+    scales: {
+      x: {
+        display: false, // Hide the x-axis grid lines
+      },
+      y: {
+        display: false, // Hide the y-axis grid lines
+      },
+    },
+    plugins: {
+      legend: {
+        display: true,
+        position: "top" as any,
+      },
+      title: {
+        display: true,
+        text: "Chart 1",
+      },
+    },
+  },
+  {
+    data: {
+      labels,
+      datasets: [
+        {
+          display: false,
+          fill: true,
+          label: "Customers",
+          data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+          backgroundColor: "rgba(35, 162, 206, 1)",
+        },
+      ],
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    height: "100%",
+    scales: {
+      x: {
+        display: false, // Hide the x-axis grid lines
+      },
+      y: {
+        display: false, // Hide the y-axis grid lines
+      },
+    },
+    plugins: {
+      legend: {
+        display: true,
+        position: "top" as any,
+      },
+      title: {
+        display: true,
+        text: "Chart 2",
+      },
+    },
+  },
+  {
+    data: {
+      labels,
+      datasets: [
+        {
+          display: false,
+          fill: true,
+          label: "Users",
+          data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+          backgroundColor: "rgba(227, 167, 60, 1)",
+        },
+      ],
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    height: "100%",
+    scales: {
+      x: {
+        display: false, // Hide the x-axis grid lines
+      },
+      y: {
+        display: false, // Hide the y-axis grid lines
+      },
+    },
+    plugins: {
+      legend: {
+        display: true,
+        position: "top" as any,
+      },
+      title: {
+        display: true,
+        text: "Chart 3",
+      },
+    },
+  },
+];
 
 const Dashboard = () => {
   return (
     <div className="w-4/5">
       <div className="h-2/5 grid grid-cols-3 gap-3">
-        <AreaChart />
-        <AreaChart />
-        <AreaChart />
+        {OPTIONS_PLACEHOLDER.map((option, index) => {
+          return <AreaChart options={option} data={option.data} key={index} />;
+        })}
       </div>
     </div>
   );
--- src/components/AreaChart/index.tsx
+++ src/components/AreaChart/index.tsx
@@ -11,7 +11,6 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
-import { faker } from "@faker-js/faker";
 
 ChartJS.register(
   CategoryScale,
@@ -23,46 +22,14 @@ ChartJS.register(
   Filler,
   Legend
 );
-const options = {
-  responsive: true,
-  maintainAspectRatio: false,
-  height: "100%",
-  scales: {
-    x: {
-      display: false, // Hide the x-axis grid lines
-    },
-    y: {
-      display: false, // Hide the y-axis grid lines
-    },
-  },
-  plugins: {
-    legend: {
-      display: true,
-      position: "top" as any,
-    },
-    title: {
-      display: true,
-      text: "Chart 1",
-    },
-  },
-};
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      display: false,
-      fill: true,
-      label: "Orders",
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: "rgba(206, 92, 96, 1)",
-    },
-  ],
-};
-
-function AreaChart() {
+interface Props {
+  options?: any;
+  data?: any;
+  label?: string;
+  text?: string;
+}
+function AreaChart({ options, data, label, text }: Props) {
   return (
     <div className="bg-white m-3 rounded-sm">
       <Line options={options} data={data} />
