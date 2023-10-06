7b657b0f93488d6a1a12a95de77d22a8021a1fbd
Amar-Omerika <72925790+Amar-Omerika@users.noreply.github.com>
2023-10-06 14:49:11 +0200

    Added responsive mobile design for navigation bar and dashboard page

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -10,16 +10,18 @@ import {
 } from "@material-tailwind/react";
 import {
   PresentationChartBarIcon,
-  UserCircleIcon,
-  Cog6ToothIcon,
-  InboxIcon,
-  PowerIcon,
   UserIcon,
+  Bars3Icon,
 } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
 const SideBar = () => {
   const [isMobile, setIsMobile] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleHambureger = () => {
+    setIsOpen((prevValue) => !prevValue);
+  };
 
   useEffect(() => {
     const handleResize = () => {
@@ -34,11 +36,44 @@ const SideBar = () => {
     };
   }, []);
   return (
-    <>
+    <div>
       {isMobile ? (
-        <nav className="w-full h-1/6 bg-green flex flex-col">
-          <p>tsst</p>
-        </nav>
+        <div className="h-[60px] grid bg-green">
+          <button onClick={handleHambureger}>
+            <Bars3Icon className="h-12 w-12 ml-auto p-1 text-white" />
+          </button>
+          {isOpen && (
+            <AnimatePresence>
+              <motion.ul
+                layout="position"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5, delay: 0.2 }}
+                className={`py-6 text-white text-center font-customfont w-full mt-10 bg-green fixed flex flex-col basis-full uppercase`}
+              >
+                <Link to={"/"}>
+                  <motion.li
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 0.5 }}
+                  >
+                    Dashboard
+                  </motion.li>
+                </Link>
+                <Link to={"/customers"}>
+                  <motion.li
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 0.5 }}
+                  >
+                    Customers
+                  </motion.li>
+                </Link>
+              </motion.ul>
+            </AnimatePresence>
+          )}
+        </div>
       ) : (
         <Card className="h-full w-full max-w-[20rem] p-4 shadow-xl rounded-none bg-green shadow-blue-gray-900/5">
           <div className="mb-2 p-4">
@@ -66,7 +101,7 @@ const SideBar = () => {
           </List>
         </Card>
       )}
-    </>
+    </div>
   );
 };
 
